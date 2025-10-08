8b84bd3bdae4ba81295d701cc90bbaf5dc861d8d
Seyko00 <Skeletosha99@gmail.com>
2025-10-08 09:14:28 +0300

    Add files via upload

---
+++ Задание 5.4/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Задание_4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                ImageView.Source = bitmap; // Установка изображения в элемент Image
+                ZoomSlider.Value = 1; // Сброс значения слайдера при открытии нового изображения
+            }
+        }
+
+        private void ZoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ImageView.Source != null)
+            {
+                ImageView.Width = ImageView.Source.Width * ZoomSlider.Value; // Изменение ширины
+                ImageView.Height = ImageView.Source.Height * ZoomSlider.Value; // Изменение высоты
+            }
+        }
+    }
+}
---
+++ Задание 5.5/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Задание_5
+{
+    public partial class MainWindow : Window
+    {
+        private double _result;
+        private string _operation;
+        private bool _isOperationPerformed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (_isOperationPerformed)
+            {
+                ResultTextBox.Text = button.Content.ToString();
+                _isOperationPerformed = false;
+            }
+            else
+            {
+                ResultTextBox.Text += button.Content.ToString();
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (double.TryParse(ResultTextBox.Text, out _result))
+            {
+                _operation = button.Content.ToString();
+                _isOperationPerformed = true;
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double secondOperand;
+
+            if (double.TryParse(ResultTextBox.Text, out secondOperand))
+            {
+                switch (_operation)
+                {
+                    case "+":
+                        _result += secondOperand;
+                        break;
+                    case "-":
+                        _result -= secondOperand;
+                        break;
+                    case "*":
+                        _result *= secondOperand;
+                        break;
+                    case "/":
+                        if (secondOperand != 0)
+                            _result /= secondOperand;
+                        else
+                            ResultTextBox.Text = "Error";
+                        break;
+                }
+                ResultTextBox.Text = _result.ToString();
+                _operation = string.Empty;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTextBox.Text = string.Empty;
+            _result = 0;
+            _operation = string.Empty;
+        }
+    }
+}
